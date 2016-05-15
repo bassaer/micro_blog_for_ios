2cb7dbe4d80a251ec1dbc3f3988daac7cccc592c
bassaer <rf.1799@gmail.com>
2016-05-15 14:41:57 +0900

    Initial Commit

---
+++ micro_blog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  micro_blog
+//
+//  Created by Nakayama on 2016/05/15.
+//  Copyright © 2016年 Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
